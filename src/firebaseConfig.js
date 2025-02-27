ec7a55ed95eff4efe49ddc28810d3814026843d9
Nikitha-gr <nikitha14.gr@gmail.com>
2025-02-27 14:12:44 +0530

    First commit: Empty

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArhryOIvMr_cLQwirFpj9NBCgqAOJC3Y0",
+  authDomain: "module4-mockeval2.firebaseapp.com",
+  projectId: "module4-mockeval2",
+  storageBucket: "module4-mockeval2.firebasestorage.app",
+  messagingSenderId: "831080184785",
+  appId: "1:831080184785:web:25cc3f515ee5b20939fef0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
